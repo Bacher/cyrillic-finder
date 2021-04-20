5121e73ae446178b8b3ce986bca831c164528673
Ilya Lebedev <bacherko@gmail.com>
2021-04-20 17:50:31 +0300

    version 0.1.0

--- src/main.js
+++ src/main.js
@@ -6,10 +6,16 @@ const glob = require('glob');
 const chalk = require('chalk');
 
 const {argv} = yargs(hideBin(process.argv))
+  .option('path', {
+    alias: 'p',
+    description: 'Run in specific directory',
+    default: process.cwd(),
+    defaultDescription: 'current directory',
+  })
   .option('ignore', {
     alias: 'i',
     description: 'Ignore pattern in glob syntax (ex: cache/**)',
-    example: '.cache/**/*',
+    exaple: '.cache/**/*',
     type: 'string',
   })
   .option('error', {
@@ -31,18 +37,50 @@ const {argv} = yargs(hideBin(process.argv))
   .alias('help', 'h')
   .alias('version', 'v')
   .example([
-    ['$0 src/', 'Use for files in src directory'],
-    ["$0 --ignore '.cache/**' src/", 'Use ignoring pattern'],
+    ["$0 '**.ts'", 'Run for .ts files'],
+    ['$0 -p src/', 'Use for files in src directory'],
+    ["$0 --ignore '.cache/**' -p src/", 'Use ignoring pattern'],
     ['$0', 'Run in current working directory'],
   ]);
 
-console.log(argv);
+const ignoreExts = ['jpg', 'jpeg', 'gif', 'png', 'zip', 'gz'];
 
-const DIR = argv._[0] || process.cwd();
+const DIR = argv.path;
 
 const colorize = !('color' in argv) || argv.color === true;
 const showFileNames = !('filename' in argv) || argv.filename === true;
 const showContent = !('content' in argv) || argv.content === true;
+
+function getGitIgnoreLines() {
+  let ignoreLines;
+
+  try {
+    ignoreLines = fs.readFileSync('.gitignore', 'utf-8');
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+
+  return ignoreLines
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => Boolean(line))
+    .map((line) => {
+      let updated = line;
+
+      if (!updated.startsWith('/')) {
+        return `**/${updated}`;
+      }
+
+      if (updated.endsWith('/')) {
+        updated += '**';
+      } else {
+        updated += '/**';
+      }
+
+      return updated;
+    });
+}
 
 function toArray(value) {
   if (value == null) {
@@ -56,76 +94,103 @@ function toArray(value) {
   return [value];
 }
 
-let someFound = false;
+function run() {
+  let errorLines = 0;
 
-glob(
-  '!(node_modules|.next|build|*cache*|.git|.idea)/**',
-  {
-    cwd: DIR,
-    ignore: ['**/*.png', '**/*.jpe?g', ...toArray(argv.ignore)],
-    dot: true,
-    nodir: true,
-  },
-  (err, files) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-      return;
-    }
+  glob(
+    argv._[0] || '**',
+    {
+      cwd: DIR,
+      ignore: [
+        '**/.git/**',
+        '**/node_modules/**',
+        'cache',
+        ...ignoreExts.map((ext) => `**/*.${ext}`),
+        ...getGitIgnoreLines(),
+        ...toArray(argv.ignore),
+      ],
+      dot: true,
+      nodir: true,
+    },
+    (err, files) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+        return;
+      }
 
-    if (files.length > 5000) {
-      console.error(`Too many files found: ${files.length}`);
-      process.exit(1);
-      return;
-    }
+      if (files.length > 5000) {
+        console.error(`Too many files found: ${files.length}`);
+        process.exit(1);
+        return;
+      }
 
-    for (const filePath of files) {
-      const content = fs.readFileSync(path.join(DIR, filePath), 'utf-8');
+      outer: for (const filePath of files) {
+        const buffer = fs.readFileSync(path.join(DIR, filePath));
 
-      const match = content.match(/[а-яёй]+/i);
+        for (let i = 0; i < buffer.length; i++) {
+          if (buffer[i] === 0) {
+            continue outer;
+          }
+        }
 
-      if (match) {
-        someFound = true;
+        const content = buffer.toString();
 
-        console.log(`File "${filePath}" contains cyrillic chars${!showFileNames && !showContent ? '' : ':'}`);
+        const match = content.match(/[а-яёй]+/i);
 
-        const lines = content.split('\n');
+        if (match) {
+          console.log(`File "${filePath}" contains cyrillic chars${!showFileNames && !showContent ? '' : ':'}`);
 
-        for (let i = 0; i < lines.length; i++) {
-          const line = lines[i];
-          const lineMatch = line.match(/[а-яёй]+(?:[\s,.:-?+]*[а-яёй]+)*/i);
+          const lines = content.split('\n');
 
-          if (lineMatch) {
-            let value = lineMatch[0];
+          for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+            const lineMatch = line.match(/[а-яёй]+(?:[\s,.:?+-]*[а-яёй]+)*/i);
 
-            if (colorize) {
-              value = chalk.bgRed(value);
-            }
+            if (lineMatch) {
+              errorLines++;
 
-            let output = '  ';
+              let value = lineMatch[0];
 
-            if (showFileNames) {
-              output += `- ${filePath}:${i + 1}:${lineMatch.index + 1}`;
-            }
+              if (colorize) {
+                value = chalk.bgRed(value);
+              }
 
-            if (showContent) {
-              const str = `${line.substr(0, lineMatch.index)}${value}${line.substr(
-                lineMatch.index + lineMatch[0].length,
-              )}`;
+              let output = '  ';
 
-              output += ` ${str}`;
-            }
+              if (showFileNames) {
+                output += `- ${filePath}:${i + 1}:${lineMatch.index + 1}`;
+              }
 
-            if (output.trim()) {
-              console.log(output);
+              if (showContent) {
+                const str = `${line.substr(0, lineMatch.index)}${value}${line.substr(
+                  lineMatch.index + lineMatch[0].length,
+                )}`;
+
+                output += ` ${str}`;
+              }
+
+              if (output.trim()) {
+                console.log(output);
+              }
+
+              if (errorLines > 100) {
+                break;
+              }
             }
           }
         }
-      }
-    }
 
-    if (someFound && argv.error) {
-      process.exit(1);
-    }
-  },
-);
+        if (errorLines > 100) {
+          break;
+        }
+      }
+
+      if (errorLines > 0 && argv.error) {
+        process.exit(1);
+      }
+    },
+  );
+}
+
+module.exports = run;
